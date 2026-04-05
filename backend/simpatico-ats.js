a929b0f9d94714f848936bfb7478633f626ac911
simpaticohr <simpaticohrconsultancy@gmail.com>
2026-04-05 10:27:45 +0530

    Fixed CORS for localhost:8000 in ATS Worker.

--- backend/simpatico-ats.js
+++ backend/simpatico-ats.js
@@ -42,7 +42,9 @@ const ALLOWED_ORIGINS = [
   'https://www.simpaticohrconsultancy.com',
   'http://localhost:3000',
   'http://localhost:5500',
+  'http://localhost:8000',
   'http://127.0.0.1:5500',
+  'http://127.0.0.1:8000',
 ];
 
 function getCorsHeaders(request) {
